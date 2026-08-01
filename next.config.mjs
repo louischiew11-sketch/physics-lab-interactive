2d8599f9d1c6ef556a6e6a7c37c3fc9543fe5c3f
Louis Chiew You Cheng <louis>
2026-08-01 18:54:50 +0800

    Fixed mobile HUD height and corrected ice sliding physics

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // this is required for static export for my to remember  
+  },
 };
 
 export default nextConfig;
